d838e2c0d96133094e7bdc84feb440b107710462
osamaebaid1973 <115580703+osamaebaid1973@users.noreply.github.com>
2025-03-05 17:48:04 +0200

    update app text style

--- lib/core/app_theme/app_text_style.dart
+++ lib/core/app_theme/app_text_style.dart
@@ -4,30 +4,30 @@ import '../app_sizes/app_font_size.dart';
 import 'app_font_weight.dart';
 
 class AppTextStyle{
-  TextStyle _getTextStyle(double fontSize,Color color,FontWeight fontWeight){
+  static TextStyle _getTextStyle(double fontSize,Color color,FontWeight fontWeight){
     return TextStyle(color: color,fontSize: fontSize,fontWeight: fontWeight);
   }
 
   //return light Text style
-  TextStyle getLightTextStyle({double fontSize = AppFontSize.s12,required Color color}){
+  static TextStyle getLightTextStyle({double fontSize = AppFontSize.s12,required Color color}){
     return _getTextStyle(fontSize,color,AppFontWeight.light);
   }
 
-  TextStyle getRegularTextStyle({double fontSize = AppFontSize.s16,required Color color}){
+  static TextStyle getRegularTextStyle({double fontSize = AppFontSize.s16,required Color color}){
     return _getTextStyle(fontSize,color,AppFontWeight.regular);
   }
 
-  TextStyle getMediumTextStyle({double fontSize = AppFontSize.s20,required Color color}){
+  static TextStyle getMediumTextStyle({double fontSize = AppFontSize.s20,required Color color}){
     return _getTextStyle(fontSize,color,AppFontWeight.medium);
   }
 
-  TextStyle getSemiBoldTextStyle({double fontSize = AppFontSize.s24,required Color color}){
+  static TextStyle getSemiBoldTextStyle({double fontSize = AppFontSize.s24,required Color color}){
     return _getTextStyle(fontSize,color,AppFontWeight.semiBold);
 
 
   }
 
-  TextStyle getBoldTextStyle({double fontSize = AppFontSize.s30,required Color color}){
+  static TextStyle getBoldTextStyle({double fontSize = AppFontSize.s30,required Color color}){
     return _getTextStyle(fontSize,color,AppFontWeight.bold);
   }
 }
