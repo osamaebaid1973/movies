0641e0d22ac28b85487466532dc3bc2aa43a1f58
osamaebaid1973 <115580703+osamaebaid1973@users.noreply.github.com>
2025-03-05 17:17:51 +0200

    create forget_password

---
+++ lib/features/authentication/forget_password/presentation/forget_password.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ForgetPassword extends StatelessWidget {
+  const ForgetPassword({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Title'),
+      ),
+      body: Container(),
+    );
+  }
+}
