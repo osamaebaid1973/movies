14c387294984f5f34fe05af97c496958c002a213
osamaebaid1973 <115580703+osamaebaid1973@users.noreply.github.com>
2025-03-05 17:37:28 +0200

    create app_bar theme

---
+++ lib/core/app_theme/app_bar_theme.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:movies/core/app_theme/app_colors.dart';
+
+AppBarTheme appBarTheme = const AppBarTheme(backgroundColor: AppColors.backgroundColor,centerTitle: true);
